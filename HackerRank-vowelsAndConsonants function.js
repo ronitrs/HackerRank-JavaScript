4b62e04ff9d6c573519f3a9b35a91d75470b6bcd
ronitrs <71811366+ronitrs@users.noreply.github.com>
2021-04-18 12:15:07 -0600

    Create HackerRank-vowelsAndConsonants function.js

---
+++ HackerRank-vowelsAndConsonants function.js
@@ -0,0 +1,12 @@
+function vowelsAndConsonants(s) {
+    let vowels='aeiou'
+    let consonants=''
+    for(let i=0; i<s.length; i++){
+        if(vowels.includes(s[i])){
+            console.log(s[i]);
+        } else {
+            consonants+= s[i] + '\n';
+        }
+    }
+    console.log(consonants.trim());
+}
